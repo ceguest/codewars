1a38e5dba906b725dc8bfda8bdeefcac34aca9b6
Claire Guest <claire.guest@madetech.com>
2020-09-17 10:57:20 +0100

    Pass test your_order_please 'order' returns an ordered two word string

--- lib/your_order_please.rb
+++ lib/your_order_please.rb
@@ -4,8 +4,11 @@ def order(words)
     word_array = words.split
     if word_array.length <= 1
       return words
-    end   
-    return "#{word_array[1]} #{word_array[0]}"
+    end
+
+    first_word = word_array.select { |word| /1/.match(word)}[0]
+    second_word = word_array.select { |word| /2/.match(word)}[0]
+    return "#{first_word} #{second_word}"
 end
 
 
--- spec/your_order_please_spec.rb
+++ spec/your_order_please_spec.rb
@@ -27,7 +27,7 @@ describe "order" do
     end
 
     ordered_two_word_inputs = [["He1llo W2orld", "He1llo W2orld"],
-                            ["1Code Wars2", "1Code Wars2"]]
+                               ["1Code Wars2", "1Code Wars2"]]
     ordered_two_word_inputs.each do |input, output|
         context "given a string with two ordered words" do
             it "returns the words in the correct position" do
