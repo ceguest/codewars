29a35c4ba387b3ffa93ca93891e919c8d53ed367
Claire Guest <claire.guest@madetech.com>
2020-09-17 11:13:51 +0100

    Refactor your_order_please 'order' to remove redundant code

--- lib/your_order_please.rb
+++ lib/your_order_please.rb
@@ -1,15 +1,10 @@
 
 def order(words)
     word_array = words.split
-    if word_array.length <= 1
-      return words
-    end
-
     ordered_words = ""
     (1..word_array.length).each do |position|
         ordered_words << word_array.select { |word| /#{position}/.match(word)}[0]
         ordered_words << " "
     end
-
     return ordered_words.strip
 end
