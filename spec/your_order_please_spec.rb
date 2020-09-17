00ecb54d4b6f1672e9c391dd71d6a422a2f3b365
Claire Guest <claire.guest@madetech.com>
2020-09-17 09:52:42 +0100

    Refactor test your_order_please 'order' returns one word string

--- spec/your_order_please_spec.rb
+++ spec/your_order_please_spec.rb
@@ -1,15 +1,14 @@
 require "your_order_please"
 
-describe "order" do
-    context "given a string with one word" do
-        it "returns the given string" do
-            expect(order("O1ne")).to eq("O1ne")
-        end
-    end
 
-    context "given a string with one word" do
-        it "returns the given string" do
-            expect(order("U1no")).to eq("U1no")
+
+describe "order" do
+    one_word_inputs = ["O1ne", "U1no"]
+    one_word_inputs.each do |string|
+        context "given a string with one word" do
+            it "returns the given string" do
+                expect(order(string)).to eq(string)
+            end
         end
     end
 end
