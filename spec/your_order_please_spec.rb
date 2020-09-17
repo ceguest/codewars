182955975af749616f86f78438b59894c8c59cc0
Claire Guest <claire.guest@madetech.com>
2020-09-17 09:47:11 +0100

    Triangulate test your_order_please 'order' returns one word string

--- spec/your_order_please_spec.rb
+++ spec/your_order_please_spec.rb
@@ -6,4 +6,10 @@ describe "order" do
             expect(order("O1ne")).to eq("O1ne")
         end
     end
+
+    context "given a string with one word" do
+        it "returns the given string" do
+            expect(order("U1no")).to eq("U1no")
+        end
+    end
 end
